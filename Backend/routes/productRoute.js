63406f6f65c464bad1a127ef93221af383fc655a
Daksh Sharma <82373462+justdaksh@users.noreply.github.com>
2022-12-10 21:11:57 +0530

    routes for Update and Delete

--- Backend/routes/productRoute.js
+++ Backend/routes/productRoute.js
@@ -1,6 +1,6 @@
 // import express as express
 const express = require("express");
-const { getAllProducts,createProduct } = require("../controllers/productController");
+const { getAllProducts,createProduct, updateProduct, deleteProduct } = require("../controllers/productController");
 
 // first make a Router to handle routes
 const router = express.Router(); 
@@ -9,8 +9,13 @@ const router = express.Router();
 router.get('/products', getAllProducts);
 
 // route to create product
-router.post('/products/new',createProduct);
+router.post('/products/new', createProduct);
 
+// route to update product
+router.put('/products/:id', updateProduct);
+
+//route to delete product
+router.delete('/products/:id', deleteProduct);
 
 
 module.exports = router;
