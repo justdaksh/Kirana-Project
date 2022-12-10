aa24ae8b990d9816c373d1faa085a539c2554f4f
Daksh Sharma <82373462+justdaksh@users.noreply.github.com>
2022-12-10 19:43:55 +0530

    Create and view products

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -1,6 +1,19 @@
-exports.getAllProducts = (req, res) => {
-    
+const Product = require("../models/productModel");
+
+// now to Create product
+exports.createProduct = async (req, res, next) => {
+    const product = await Product.create(req.body)
+    res.status(201).json({
+        success: true,
+        product
+    })
+}
+
+// to get all products
+exports.getAllProducts = async(req, res) => {
+    const products = await Product.find();
     res.status(200).json({
-        message: "route is working fine"
+        success: true,
+        products
     })
 }
