43aa93dc8ae721831967746a15d7ce898b0c7d74
Daksh Sharma <82373462+justdaksh@users.noreply.github.com>
2022-12-10 13:29:08 +0530

    server working

---
+++ Backend/server.js
@@ -0,0 +1,19 @@
+// importing the exported app from express
+const app = require("./app");
+
+// Import the connectdatabse
+const connectDatabase = require("./config/database");
+
+// importing dotenv
+const dotenv = require("dotenv");
+
+// config
+dotenv.config({ path: "Backend/config/config.env" });
+
+// connect with database in server after imporing config
+connectDatabase()
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`server is Working on http://localhost:${process.env.PORT}`);
+})
