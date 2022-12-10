dd5ee2290d1a6ad4d380f93027801cac6f154c3f
Daksh Sharma <82373462+justdaksh@users.noreply.github.com>
2022-12-10 21:11:03 +0530

    casual changes

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 mongoose.set('strictQuery', true); //done to handle the mongoose 7 strictquery 
 // Connecting databse
 const connectDatabse = () => {
-    mongoose.connect(process.env.DB_URI).then((data) => {
+    mongoose.connect(process.env.DB_URI,{useNewUrlParser: true}).then((data) => {
         console.log(`MongoDB connected to server : ${data.connection.host}`);
     }).catch((err) => {
         console.log(err);
