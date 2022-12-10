01465a8f1dad8c460cd556dd4d85de4beaf8fe64
Daksh Sharma <82373462+justdaksh@users.noreply.github.com>
2022-12-10 19:47:28 +0530

    Handeled formatting

--- Backend/server.js
+++ Backend/server.js
@@ -7,14 +7,11 @@ const connectDatabase = require("./config/database");
 // importing dotenv
 const dotenv = require("dotenv");
 
-
-
 // config
 dotenv.config({ path: "Backend/config/config.env" });
 
 // connect with database in server after imporing config
 connectDatabase()
-
 
 // TO make the server running
 app.listen(process.env.PORT, () => {
