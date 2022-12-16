679799dea07cdff5552395867931a3495233fc7b
Daksh Sharma <82373462+justdaksh@users.noreply.github.com>
2022-12-16 22:40:47 +0530

    Handeling various errors

--- Backend/server.js
+++ Backend/server.js
@@ -13,7 +13,24 @@ dotenv.config({ path: "Backend/config/config.env" });
 // connect with database in server after imporing config
 connectDatabase()
 
+// Handling Uncaught Exception #keep it about all console logs
+process.on("uncaughtException", err => {
+    console.log(`Error: ${err.message}`);
+    console.log(`Shutting Down Server Due to Uncaught Exception`);
+    process.exit(1);
+})
+
+
+
 // TO make the server running
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
     console.log(`server is Working on http://localhost:${process.env.PORT}`);
 })
+// Unhandeled Promise Rejection
+process.on("unhandledRejection", err => {
+    console.log(`Error: ${err.message}`);
+    console.log(`Shutting Down the Server Due to Unhandeled Promise Rejection`);
+    server.close(() => {
+        process.exit(1);
+    });
+});
