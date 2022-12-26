19ca66d4349da1b27ccffbd2806d32b054326f97
Daksh Sharma <82373462+justdaksh@users.noreply.github.com>
2022-12-26 21:41:20 +0530

    adding auth to Create Update and Delete Routes

--- Backend/routes/productRoute.js
+++ Backend/routes/productRoute.js
@@ -1,6 +1,7 @@
 // import express as express
 const express = require("express");
 const { getAllProducts,createProduct, updateProduct, deleteProduct, getProductDetails } = require("../controllers/productController");
+const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 
 // first make a Router to handle routes
 const router = express.Router(); 
@@ -9,10 +10,10 @@ const router = express.Router();
 router.get('/products', getAllProducts);
 
 // route to create product
-router.post('/products/new', createProduct);
+router.post('/products/new',isAuthenticatedUser,authorizeRoles("admin"), createProduct);
 
 // route to update || delete || get detail of single product
-router.put('/products/:id', updateProduct).delete('/products/:id', deleteProduct).get("/products/:id", getProductDetails);
+router.put('/products/:id',isAuthenticatedUser,authorizeRoles("admin"), updateProduct).delete('/products/:id',isAuthenticatedUser,authorizeRoles("admin"), deleteProduct).get("/products/:id", getProductDetails);
 
 
 
