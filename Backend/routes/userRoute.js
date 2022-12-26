577171e26464a733fbf614d62e9c5158f209f775
Daksh Sharma <82373462+justdaksh@users.noreply.github.com>
2022-12-26 21:43:18 +0530

    logoutUser route

--- Backend/routes/userRoute.js
+++ Backend/routes/userRoute.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { registerUser, loginUser } = require("../controllers/userController");
+const { registerUser, loginUser, logoutUser } = require("../controllers/userController");
 const router = express.Router();
 
 router.post('/register', registerUser); // route to register user
 
 router.post('/login', loginUser); // route to login user
+
+router.post('/logout', logoutUser);//route to logut User
 
 module.exports = router;
