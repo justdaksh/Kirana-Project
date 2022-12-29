6d77c6ecd8ae21ff873e625513472642f6f3f714
Daksh Sharma <82373462+justdaksh@users.noreply.github.com>
2022-12-29 19:59:45 +0530

    Added routes for forgot and reset

--- Backend/routes/userRoute.js
+++ Backend/routes/userRoute.js
@@ -1,10 +1,14 @@
 const express = require("express");
-const { registerUser, loginUser, logoutUser } = require("../controllers/userController");
+const { registerUser, loginUser, logoutUser, forgotPassword, resetPassword } = require("../controllers/userController");
 const router = express.Router();
 
 router.post('/register', registerUser); // route to register user
 
 router.post('/login', loginUser); // route to login user
+
+router.post('/password/forgot', forgotPassword) // route for forgot password
+
+router.put('/password/reset/:token',resetPassword) // route to reset password
 
 router.post('/logout', logoutUser);//route to logut User
 
