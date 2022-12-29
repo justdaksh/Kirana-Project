5dfebe37bc68255ca4122954a9e55de24733573e
Daksh Sharma <82373462+justdaksh@users.noreply.github.com>
2022-12-29 21:58:09 +0530

    Error Handeling for JWT and Mongoose

--- Backend/middleware/error.js
+++ Backend/middleware/error.js
@@ -5,9 +5,26 @@ module.exports = (err,req,res,next) => {
     err.statusCode = err.statusCode || 500;
     err.message = err.message || "Intenal Server Error"
 
-// Mongo Db error
+    // Mongo Db error
     if (err.name === "CastError") {
         const message = `Respurce not found : ${err.path}`;
+        err = new ErrorHandler(message, 400);
+    }
+    // Mongoose Duplicate key Error
+    if (err.code == 11000) {
+        const message = `Duplicate ${Object.keys(err.keyValue)} entered`
+        err = new ErrorHandler(message, 400);
+    }
+    // Wrong JWT error
+    if (err.name === "JsonWebTokenError") {
+        
+        const message = `Json Web Token is Invalid, try Again`;
+        err = new ErrorHandler(message, 400);
+    }
+    // JWT Expire Error
+    if (err.name === "TokenExpiredError") {
+        
+        const message = `Json Web Token is Expired, try Again`;
         err = new ErrorHandler(message, 400);
     }
 
