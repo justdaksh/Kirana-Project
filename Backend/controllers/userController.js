7dc1856d0485f7987311cccf4705b0320748868f
Daksh Sharma <82373462+justdaksh@users.noreply.github.com>
2022-12-29 19:59:07 +0530

    Forgot and Reset Password

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -3,6 +3,8 @@ const catchAsyncErrors = require("../middleware/catchAsyncError");
 const User = require("../models/userModel");
 const { findOne } = require("../models/userModel");
 const sendToken = require("../utils/jwtToken");
+const sendEmail = require("../utils/sendEmail");
+const crypto = require("crypto")
 
 //Register a User
 exports.registerUser = catchAsyncErrors(async (req, res, next) => {
@@ -56,4 +58,74 @@ exports.logoutUser = catchAsyncErrors(async (req, res, next) => {
         success: true,
         message: "Logged Out",
     });
+})
+
+// Forgot Password
+exports.forgotPassword = catchAsyncErrors(async (req, res, next) => { 
+    // Fetching the Email of the client
+    const user = await User.findOne({ email: req.body.email });
+    //check if any user of given email present
+    if (!user) {
+        return next(new Errorhandler("User not found", 404));
+    }
+    // if found get the reset token
+    const resetToken =await user.getResetPasswordToken();
+
+    await user.save({ validateBeforeSave: false }); // to save the changes made on resetPsswordExpire and resetPasswordToken in DB
+    
+    // URL that will be sent through email
+    const resetPasswordUrl = `${req.protocol}://${req.get("host")}/api/v1/password/reset/${resetToken}`;
+
+    const message = `Your Password Reset Token is :- \n\n ${resetPasswordUrl} \n\n If you have note requested this email then, Please ignore it.`
+    try { // try sending the Email
+        await sendEmail({
+            email:  user.email,
+            subject: `Kirana Password Recovery`,
+            message,
+
+        });
+        res.status(200).json({
+            success: true,
+            message: `Email sent to ${user.email} successfully!`
+            
+        })
+        
+    } catch (error) { // If Email can't be sent immediately make variables undefined in databse
+        user.resetPasswordToken = undefined;
+        user.resetPasswordExpire = undefined;
+
+        await user.save({ validateBeforeSave: false }); 
+        return next(new Errorhandler(error.message, 500));
+    }
+
+})
+
+//Reset Password
+exports.resetPassword = catchAsyncErrors(async (req, res, next) => {
+
+    //creating token hash
+    const resetPasswordToken = crypto.createHash("sha256").update(req.params.token).digest("hex"); //making hash of the token
+    
+    const user = await User.findOne({
+        resetPasswordExpire : {$gt:Date.now()}, // check if the oken have not expired
+        resetPasswordToken
+    })
+     // if no user found
+    if (!user) {
+        return next(new Errorhandler("Reset Password Token is invalid or has been expired.", 404));
+    }
+     // check for both params to be same pass = confirmPass
+    if (req.body.password !== req.body.confirmPassword) {
+        return next(new Errorhandler("Password does not match.", 404));
+    }
+    // Change In Databses
+    user.password = req.body.password; 
+    user.resetPasswordExpire = undefined;
+    user.resetPasswordToken = undefined;
+
+    // Save in database
+    await user.save(); 
+
+    // Login at the same time and save cookie
+    sendToken(user, 200, res); 
 })
