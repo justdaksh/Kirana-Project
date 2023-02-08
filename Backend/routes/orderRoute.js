f17b6e771b842aae0f3bdb28caaff11e255a3d10
Daksh Sharma <82373462+justdaksh@users.noreply.github.com>
2023-02-08 20:35:43 +0530

    Adding OrderApi routes

--- Backend/routes/orderRoute.js
+++ Backend/routes/orderRoute.js
@@ -1,10 +1,13 @@
 const express = require("express");
-const { newOrder } = require("../controllers/orderController");
+const { newOrder, myOrders, getSingleOrder, getAllOrders, updateOrder, deleteOrder } = require("../controllers/orderController");
 const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 const router = express.Router();
 
 // route to create newOrder
 router.post('/order/new',isAuthenticatedUser, newOrder);
-
-
+router.get('/orders/me',isAuthenticatedUser, myOrders);
+router.get('/order/:id', isAuthenticatedUser, getSingleOrder)
+router.get('/admin/orders', isAuthenticatedUser, authorizeRoles("admin"), getAllOrders)
+    .put('/admin/order/:id', isAuthenticatedUser, authorizeRoles("admin"), updateOrder)
+    .delete('/admin/order/:id',isAuthenticatedUser,authorizeRoles("admin"),deleteOrder)
 module.exports = router;
